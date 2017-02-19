c90157045c80af9d4c4a9f26238f985f22b0bdce
r12f <r12f.code@gmail.com>
2017-02-18 19:21:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# hexo-post-series
+
+[![npm version](https://badge.fury.io/js/hexo-post-series.svg)](http://badge.fury.io/js/hexo-post-series)
+
+## Installation
+
+``` bash
+$ npm install hexo-post-series --save
+```
+
+## Usage
+
+### Add post to a series
+Adding post to a series is very easy, just put ```series: series-title-you-like``` to the post info section. For example:
+```
+---
+title: A test post
+tags:
+  - tag1
+  - tag2
+series: test-posts
+---
+```
+
+### Series list and styles
+The post series list is constructed as following. To change its appearance, we can set the css styles in your theme.
+
+```
+<div class="post-series">
+    <div class="post-series-title">{post series list title}</div>
+    <ul class="post-series-list">
+        <li class="post-series-list-item"><a href="{post_link}">{post title}</a></li>
+    </ul>
+</div>
+```
+
+### Configurations
+The configurations we supported now are quite simple.
+* list_title: Specify the title you'd like to show before the list. If list title is empty, we won't generate the post-series-title div.
+* open_in_new_tab: If true, a ```target="_blank"``` will be put into the anchor.
+* reverse_sort: If true, the most recent created post will be put at first, otherwise, the least recent create post.
+
+Here is a sample:
+```
+# Post series
+post_series:
+    list_title: "Same Series:"
+    open_in_new_tab: false
+    reverse_sort: false
+```
+
+## License
+
+BSD v3
---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var log = hexo.log || log.log;
+
+hexo.extend.filter.register('before_post_render', function(postInfo) {
+    var config = hexo.config.post_series;
+    var posts = hexo.locals.get('posts');
+
+    var postsInSameSeries = posts.data.reduce(function(result, post) {
+        if (post.series && post.series === postInfo.series) {
+            result.push({
+                'id' : post.id,
+                'title' : post.title,
+                'path' : post.path,
+                'permalink' : post.permalink,
+                'date' : post.date
+            });
+        }
+        return result;
+    }, []);
+
+    postsInSameSeries.sort(function(left, right) {
+        if (config.reverse_sort) {
+            return left.date < right.date;
+        }
+        return left.date > right.date;
+    });
+
+    postInfo.postsInSameSeries = postsInSameSeries;
+
+    // console.log(postInfo.postsInSameSeries);
+    if (postInfo.postsInSameSeries.size > 0) {
+        log.log("Post series updated: " + postInfo.path);
+    }
+});
+
+hexo.extend.tag.register('post_series', function (args) {
+    var postInfo = this;
+
+    if (postInfo.postsInSameSeries.size === 0) {
+        return '';
+    }
+
+    var config = hexo.config.post_series;
+
+    var postSeriesHtmlSegments = ['<div class="post-series">'];
+    if (config.list_title) {
+        postSeriesHtmlSegments = postSeriesHtmlSegments.concat([ '<div class="post-series-title">', config.list_title, '</div>' ]);
+    }
+    postSeriesHtmlSegments.push('<ul class="post-series-list">');
+
+    postInfo.postsInSameSeries.reduce(function(result, post) {
+        result.push('<li class="post-series-list-item"><a href="');
+        result.push(isRunningInLocalServerMode() ? '/' + post.path : post.permalink);
+        result.push(config.open_in_new_tab ? '" target="_blank">' : '">');
+        result.push(post.title);
+        result.push('</a></li>');
+        return result;
+    }, postSeriesHtmlSegments);
+
+    postSeriesHtmlSegments.push('</ul></div>');
+
+    var postSeriesHtml = postSeriesHtmlSegments.join('');
+    return postSeriesHtml;
+});
+
+function isRunningInLocalServerMode() {
+   return process.argv.indexOf('server') > -1 || process.argv.indexOf('s') > -1;
+}
