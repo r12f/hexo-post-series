7b7b1c540217c970a97aeed18a30edeadb786faf
r12f <r12f.code@gmail.com>
2019-12-31 01:16:56 -0800

    Update build revision version script for linux.

--- Build/Update-BuildRevisionVersion.ps1
+++ Build/Update-BuildRevisionVersion.ps1
@@ -7,14 +7,19 @@ function Update-BuildRevisionVersion()
 {
     $buildConfigFolder = $PSScriptRoot
 
-    $packageJsonFilePath = "$buildConfigFolder\..\package.json"
+    $packageJsonFilePath = [System.IO.Path]::Combine($buildConfigFolder, "..", "package.json")
     Write-Host "package.json file path: $packageJsonFilePath"
 
     $newVersion = "$Version"
+    Write-Host "Updating version to: $newVersion"
     $packageJsonFileContent = Get-Content $packageJsonFilePath -Raw
     $packageJsonFileContent = $packageJsonFileContent.Replace("`"1.0.0`"", "`"$Version`"")
-    Write-Host "Updating version to: $newVersion"
 
+    Write-Host "New package.json content:"
+    Write-Host $packageJsonFileContent
+    Write-Host ""
+
+    Write-Host "Updating package.json: $packageJsonFilePath"
     $encoding = New-Object System.Text.UTF8Encoding $False
     [System.IO.File]::WriteAllLines($packageJsonFilePath, $packageJsonFileContent, $encoding)
 }
