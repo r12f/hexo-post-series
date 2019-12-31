547bf80ee6aa3fd1eb9e21a472db4a79a303d52a
r12f <r12f.code@gmail.com>
2019-12-31 00:36:43 -0800

    Fix post series not generated issue during clean generate.

--- index.js
+++ index.js
@@ -2,54 +2,60 @@
 
 var log = hexo.log || log.log;
 
-hexo.extend.filter.register('before_post_render', function(postInfo) {
+var postSeriesToPostMap = {}
+
+hexo.extend.filter.register('before_generate', function(){
+    postSeriesToPostMap = {}
+});
+
+hexo.extend.filter.register('before_post_render', function (postInfo) {
+    if (!postSeriesToPostMap[postInfo.series]) {
+        postSeriesToPostMap[postInfo.series] = [];
+    }
+    // console.log(postInfo);
+
+    if (!postInfo.published) {
+        return;
+    }
+
+    postSeriesToPostMap[postInfo.series].push({
+        'id': postInfo.id,
+        'title': postInfo.title,
+        'path': postInfo.path,
+        'permalink': postInfo.permalink,
+        'date': postInfo.date.unix()
+    });
+});
+
+hexo.extend.tag.register('posts_in_same_series', function (args) {
     var config = hexo.config.post_series;
-    var posts = hexo.locals.get('posts');
+    var postInfo = this;
 
-    var postsInSameSeries = posts.data.reduce(function(result, post) {
-        if (post.series && post.series === postInfo.series) {
-            result.push({
-                'id' : post.id,
-                'title' : post.title,
-                'path' : post.path,
-                'permalink' : post.permalink,
-                'date' : post.date
-            });
-        }
-        return result;
-    }, []);
+    var postsInSameSeries = postSeriesToPostMap[postInfo.series];
+    log.d(postsInSameSeries.length + " posts found in the same series for post: " + postInfo.path);
 
-    postsInSameSeries.sort(function(left, right) {
+    if (postsInSameSeries.length === 0) {
+        return '';
+    }
+
+    log.log(postsInSameSeries.length + " posts found in the same series, post series updated: " + postInfo.path);
+
+    postsInSameSeries.sort(function (left, right) {
         if (config.reverse_sort) {
             return left.date < right.date;
         }
         return left.date > right.date;
     });
 
-    postInfo.postsInSameSeries = postsInSameSeries;
-
-    // console.log(postInfo.postsInSameSeries);
-    if (postInfo.postsInSameSeries.size > 0) {
-        log.log("Post series updated: " + postInfo.path);
-    }
-});
-
-hexo.extend.tag.register('posts_in_same_series', function (args) {
-    var postInfo = this;
-
-    if (postInfo.postsInSameSeries.size === 0) {
-        return '';
-    }
-
     var config = hexo.config.post_series;
 
     var postSeriesHtmlSegments = ['<div class="post-series">'];
     if (config.list_title) {
-        postSeriesHtmlSegments = postSeriesHtmlSegments.concat([ '<div class="post-series-title">', config.list_title, '</div>' ]);
+        postSeriesHtmlSegments = postSeriesHtmlSegments.concat(['<div class="post-series-title">', config.list_title, '</div>']);
     }
     postSeriesHtmlSegments.push('<ul class="post-series-list">');
 
-    postInfo.postsInSameSeries.reduce(function(result, post) {
+    postsInSameSeries.reduce(function (result, post) {
         result.push('<li class="post-series-list-item"><a href="');
         result.push(isRunningInLocalServerMode() ? '/' + post.path : post.permalink);
         result.push(config.open_in_new_tab ? '" target="_blank">' : '">');
@@ -65,5 +71,5 @@ hexo.extend.tag.register('posts_in_same_series', function (args) {
 });
 
 function isRunningInLocalServerMode() {
-   return process.argv.indexOf('server') > -1 || process.argv.indexOf('s') > -1;
+    return process.argv.indexOf('server') > -1 || process.argv.indexOf('s') > -1;
 }
